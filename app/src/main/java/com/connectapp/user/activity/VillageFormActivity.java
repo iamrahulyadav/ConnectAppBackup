0c29299693d6d3e596a9b387816901abe8c15b17
raisahab-ritwik <ritwikrai04@gmail.com>
2018-09-04 18:01:10 +0530

    Take and submit picture done.

--- app/src/main/java/com/connectapp/user/activity/VillageFormActivity.java
+++ app/src/main/java/com/connectapp/user/activity/VillageFormActivity.java
@@ -1,10 +1,22 @@
 package com.connectapp.user.activity;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +28,9 @@ import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.connectapp.user.R;
+import com.connectapp.user.adapter.ImageAdapter;
 import com.connectapp.user.constant.Consts;
+import com.connectapp.user.data.ImageClass;
 import com.connectapp.user.data.Spouse;
 import com.connectapp.user.util.Util;
 import com.connectapp.user.view.DropDownViewForXML;
@@ -24,10 +38,19 @@ import com.connectapp.user.volley.PostWithJsonWebTask;
 import com.connectapp.user.volley.ServerResponseCallback;
 import com.connectapp.user.volley.ServerStringResponseCallback;
 import com.connectapp.user.volley.VolleyTaskManager;
+import com.google.android.gms.drive.events.CompletionEvent;
+import com.google.android.gms.games.quest.Quest;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class VillageFormActivity extends AppCompatActivity implements ServerResponseCallback, View.OnClickListener {
@@ -47,6 +70,15 @@ public class VillageFormActivity extends AppCompatActivity implements ServerResp
     private int year = 2000, day = 01, tempMonth = 01;
     private View selectedView;
 
+    private ProgressDialog mProgressDialog;
+    private final int CAMERA_PIC_REQUEST = 7464;
+    private static Uri mCapturedImageURI;
+
+    private String TAG = getClass().getSimpleName();
+    private ArrayList<ImageClass> imagesList = new ArrayList<>();
+    private String imageFileName = "";
+    private int imageCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +88,13 @@ public class VillageFormActivity extends AppCompatActivity implements ServerResp
         districtName = getIntent().getStringExtra("district");
         villageName = getIntent().getStringExtra("village");
         initView();
+        // Initialize volley
         volleyTaskManager = new VolleyTaskManager(mContext);
+        // Initialize progress dialog
+        mProgressDialog = new ProgressDialog(this.mContext);
+        mProgressDialog.setProgressStyle(0);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setCanceledOnTouchOutside(false);
     }
 
     /**
@@ -140,13 +178,7 @@ public class VillageFormActivity extends AppCompatActivity implements ServerResp
     }
 
     public void onPostClicked(View view) {
-
         validateAndPostFormData();
-
-    }
-
-    public void onCameraClicked(View view) {
-
     }
 
 
@@ -271,8 +303,6 @@ public class VillageFormActivity extends AppCompatActivity implements ServerResp
             String pickedDate = selectedYear + "-" + monthPadding + "-" + dayPading;
 //			sendingDate = monthPadding + "/" + dayPading + "/" + selectedYear;
             ((TextView) selectedView).setText(pickedDate);
-            Log.v("DatePickerDialog", pickedDate);
-
             if (selectedView.getId() == R.id.tv_dom) {
                 Toast.makeText(mContext, "Add Spouse", Toast.LENGTH_SHORT).show();
                 ll_spouse.setVisibility(View.VISIBLE);
@@ -281,5 +311,143 @@ public class VillageFormActivity extends AppCompatActivity implements ServerResp
         }
     };
 
+    public void onCameraClicked(View view) {
+        if (imagesList.size() < 1) {
+            mProgressDialog.setMessage("Please wait...");
+            mProgressDialog.setCancelable(true);
+            showProgressDialog();
+            cameraSelectedPic();
+            return;
+        }
+        Util.showMessageWithOk(VillageFormActivity.this, "Maximum number of images has already been selected!");
+    }
 
+    private void showProgressDialog() {
+        if (!mProgressDialog.isShowing()) {
+            mProgressDialog.show();
+        }
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    protected void cameraSelectedPic() {
+        //camera stuff
+        Intent imageIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        //folder stuff
+        File imagesFolder = new File(Environment.getExternalStorageDirectory(), "ConnectAppImages");
+        imagesFolder.mkdirs();
+
+        File image = new File(imagesFolder, "VILLAGE_" + timeStamp + ".png");
+        imageFileName = "VILLAGE_" + timeStamp + ".png";
+        Uri uriSavedImage = Uri.fromFile(image);
+        mCapturedImageURI = uriSavedImage;
+        imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+        startActivityForResult(imageIntent, CAMERA_PIC_REQUEST);
+    }
+
+    private void processImagePath(String picturePath) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inScaled = true;
+        int bitWidth = BitmapFactory.decodeFile(picturePath).getWidth();
+        int bitHeight = BitmapFactory.decodeFile(picturePath).getHeight();
+        System.out.println("width : " + bitWidth + " bitHeight : " + bitHeight);
+        if (bitWidth <= AccessibilityNodeInfoCompat.ACTION_PREVIOUS_HTML_ELEMENT || bitHeight <= 1536) {
+            opt.inSampleSize = 4;
+        } else if ((bitHeight <= 1536 || bitHeight > 1944)
+                && (bitWidth <= AccessibilityNodeInfoCompat.ACTION_PREVIOUS_HTML_ELEMENT || bitWidth > 2592)) {
+            opt.inSampleSize = 8;
+        } else {
+            opt.inSampleSize = 6;
+        }
+        Bitmap bitmap = BitmapFactory.decodeFile(picturePath, opt);
+        if (bitmap != null) {
+            try {
+                int orientation = new ExifInterface(picturePath).getAttributeInt("Orientation", 1);
+                Log.e("orientation", new StringBuilder(String.valueOf(orientation)).append("<<<").toString());
+                Matrix matrix = new Matrix();
+                switch (orientation) {
+                    case CompletionEvent.STATUS_FAILURE /*1*/:
+                        Log.e("Case:", "1");
+                        break;
+                    case CompletionEvent.STATUS_CONFLICT /*2*/:
+                        Log.e("Case:", "2");
+                        break;
+                    case CompletionEvent.STATUS_CANCELED /*3*/:
+                        Log.e("Case:", "3");
+                        matrix.postRotate(BitmapDescriptorFactory.HUE_CYAN);
+                        break;
+                    case GeofencingRequest.INITIAL_TRIGGER_DWELL /*4*/:
+                        Log.e("Case:", "4");
+                        break;
+                    case DetectedActivity.TILTING /*5*/:
+                        Log.e("Case:", "5");
+                        break;
+                    case Quest.STATE_FAILED /*6*/:
+                        Log.e("Case:", "6");
+                        matrix.postRotate(90.0f);
+                        break;
+                    case DetectedActivity.WALKING /*7*/:
+                        Log.e("Case:", "7");
+                        break;
+                    case DetectedActivity.RUNNING /*8*/:
+                        Log.e("Case:", "8");
+                        matrix.postRotate(-90.0f);
+                        break;
+                }
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageClass imageClass = new ImageClass();
+            imageClass.setBase64value(Util.getBase64StringFromBitmap(bitmap));
+            imageClass.setImageCount(this.imageCount + 1);
+            imagesList.add(imageClass);
+            imageCount++;
+        } else {
+            Toast.makeText(this, new StringBuilder(String.valueOf(picturePath)).append("not found").toString(), Toast.LENGTH_LONG).show();
+        }
+        imageUpdateOnView();
+    }
+
+    private void imageUpdateOnView() {
+        vp_selectedImages.setAdapter(new ImageAdapter(this, imagesList));
+        if (imagesList.size() == 0) {
+            vp_selectedImages.setBackgroundResource(R.drawable.default_empty);
+            //ib_reject_picture.setVisibility(View.INVISIBLE);
+        } else {
+            vp_selectedImages.setBackgroundColor(Color.parseColor("#D7D7D7"));
+            vp_selectedImages.setCurrentItem(imagesList.size() - 1);
+            //ib_reject_picture.setVisibility(View.VISIBLE);
+        }
+        if (imagesList.size() <= 1) {
+            tv_imageProgress.setText("[Image added " + imagesList.size() + "/1]");
+            // v_swipeRight.setVisibility(View.INVISIBLE);
+            //v_swipeLeft.setVisibility(View.INVISIBLE);
+            return;
+        }
+        tv_imageProgress.setText("Slide to view other images\n[Images added " + imagesList.size() + "/1]");
+        // v_swipeRight.setVisibility(View.VISIBLE);
+        //v_swipeLeft.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        hideProgressDialog();
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                String picturePath = mCapturedImageURI.getPath();
+                processImagePath(picturePath);
+            } else {
+                Toast.makeText(this.mContext, "No image selected", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 }
